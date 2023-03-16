bd4399d412963932dd9bdbf3dee52c3210eb840e
Aghasi Poghosyan <agasy18@gmail.com>
2023-03-16 13:50:17 +0000

    ready for testing

--- whisper_app.py
+++ whisper_app.py
@@ -1,17 +1,28 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, UploadFile
 import whisper_timestamped as whisper
 from pydantic import BaseModel
+from os import getenv
+from tempfile import NamedTemporaryFile
+from tqdm import tqdm
 
 app = FastAPI()
 
-class TranscriptionRequest(BaseModel):
-    audio_file: str
-    language: str
+model = whisper.load_model(getenv('MODEL') or "small", device=getenv('DEVICE') or "cuda")
 
+@app.post("/transcribe")
+async def get_whisper_result(audio: UploadFile, language: str = 'auto', refine_whisper_precision: float=0.7):
+    
+    with NamedTemporaryFile() as f:
+        with tqdm(total=audio.size, desc='Featching file') as pbar:
+            while content := await audio.read(1024):
+                f.write(content)
+                pbar.update(len(content))
+        f.seek(0)
+        arr = whisper.load_audio(f.name)
+    
+    if language == 'auto':
+        language = None
 
-@app.post("/whisper")
-async def get_whisper_result(request: TranscriptionRequest):
-    audio = whisper.load_audio(request.audio_file)
-    model = whisper.load_model("tiny", device="cpu")
-    result = whisper.transcribe(model, audio, language=request.language)
+    result = whisper.transcribe(model, arr, language=language, refine_whisper_precision=refine_whisper_precision)
     return result
